32782cb68a7df0f84fc77a53af3c9f300b3dd44e
mac <mac@macs-MacBook-Air.local>
2025-09-09 21:51:15 +0100

    faq page style fix

--- wp-content/themes/buzzerplayer/page.php
+++ wp-content/themes/buzzerplayer/page.php
@@ -15,21 +15,16 @@
 get_header();
 ?>
 
+
 	<main id="primary" class="site-main">
-
-		<?php
-		while ( have_posts() ) :
-			the_post();
-
-			get_template_part( 'template-parts/content', 'page' );
-
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-
-		endwhile; // End of the loop.
-		?>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<h1 class="page-title"><?= the_title() ?></h1>
+					<?= the_content() ?>
+				</div>
+			</div>
+		</div>
 
 	</main><!-- #main -->
 
